e777e2aceae3275b7d44576ec1441205769cd0f9
williamhm412 <williamhm412@gmail.com>
2025-10-29 01:58:40 -0600

    update ppt and week 2 code

---
+++ week2/pincell.py
@@ -0,0 +1,129 @@
+import openmc
+
+uo2 = openmc.Material(1, "uo2")
+mat = openmc.Material()
+
+uo2.add_nuclide('U235', 0.03)
+uo2.add_nuclide('U238', 0.97)
+uo2.add_nuclide('O16', 2.0)
+
+uo2.set_density('g/cm3', 10.0)
+
+zirconium = openmc.Material(name="zirconium")
+zirconium.add_element('Zr', 1.0)
+zirconium.set_density('g/cm3', 6.6)
+
+water = openmc.Material(name="h2o")
+water.add_nuclide('H1', 2.0)
+water.add_nuclide('O16', 1.0)
+water.set_density('g/cm3', 1.0)
+
+water.add_s_alpha_beta('c_H_in_H2O')
+
+materials = openmc.Materials([uo2, zirconium, water])
+materials = openmc.Materials()
+materials.append(uo2)
+materials += [zirconium, water]
+
+materials.export_to_xml()
+
+
+water.remove_nuclide('O16')
+water.add_element('O', 1.0)
+
+materials.export_to_xml()
+
+uo2_three = openmc.Material()
+uo2_three.add_element('U', 1.0, enrichment=3.0)
+uo2_three.add_element('O', 2.0)
+uo2_three.set_density('g/cc', 10.0)
+
+# Create PuO2 material
+puo2 = openmc.Material()
+puo2.add_nuclide('Pu239', 0.94)
+puo2.add_nuclide('Pu240', 0.06)
+puo2.add_nuclide('O16', 2.0)
+puo2.set_density('g/cm3', 11.5)
+
+# Create the mixture
+mox = openmc.Material.mix_materials([uo2, puo2], [0.97, 0.03], 'wo')
+
+sphere = openmc.Sphere(r=1.0)
+
+inside_sphere = -sphere
+outside_sphere = +sphere
+
+z_plane = openmc.ZPlane(0)
+northern_hemisphere = -sphere & +z_plane
+
+cell = openmc.Cell()
+cell.region = northern_hemisphere
+cell.fill = water
+
+universe = openmc.Universe()
+universe.add_cell(cell)
+
+fuel_outer_radius = openmc.ZCylinder(r=0.39)
+clad_inner_radius = openmc.ZCylinder(r=0.40)
+clad_outer_radius = openmc.ZCylinder(r=0.46)
+
+fuel_region = -fuel_outer_radius
+gap_region = +fuel_outer_radius & -clad_inner_radius
+clad_region = +clad_inner_radius & -clad_outer_radius
+
+fuel = openmc.Cell(name='fuel')
+fuel.fill = uo2
+fuel.region = fuel_region
+
+gap = openmc.Cell(name='air gap')
+gap.region = gap_region
+
+clad = openmc.Cell(name='clad')
+clad.fill = zirconium
+clad.region = clad_region
+
+pitch = 1.26
+left = openmc.XPlane(-pitch/2, boundary_type='reflective')
+right = openmc.XPlane(pitch/2, boundary_type='reflective')
+bottom = openmc.YPlane(-pitch/2, boundary_type='reflective')
+top = openmc.YPlane(pitch/2, boundary_type='reflective')
+
+water_region = +left & -right & +bottom & -top & +clad_outer_radius
+
+moderator = openmc.Cell(name='moderator')
+moderator.fill = water
+moderator.region = water_region
+
+box = openmc.model.RectangularPrism(width=pitch, height=pitch, boundary_type='reflective')
+
+water_region = -box & +clad_outer_radius
+
+root_universe = openmc.Universe(cells=(fuel, gap, clad, moderator))
+
+geometry = openmc.Geometry()
+geometry.root_universe = root_universe
+geometry.export_to_xml()
+# Create a point source
+point = openmc.stats.Point((0, 0, 0))
+source = openmc.IndependentSource(space=point)
+
+settings = openmc.Settings()
+settings.source = source
+settings.batches = 100
+settings.inactive = 10
+settings.particles = 1000
+
+settings.export_to_xml()
+
+cell_filter = openmc.CellFilter(fuel)
+
+tally = openmc.Tally(1)
+tally.filters = [cell_filter]
+
+tally.nuclides = ['U235']
+tally.scores = ['total', 'fission', 'absorption', '(n,gamma)']
+
+tallies = openmc.Tallies([tally])
+tallies.export_to_xml()
+
+openmc.run()
